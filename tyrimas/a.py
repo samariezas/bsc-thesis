8c6255148c8ae5a1d20d5e71628f5296d1db1eff
Joris Pevcevičius <joris.pevcas@gmail.com>
2026-05-14 22:27:13 +0300

    guest results

--- tyrimas/a.py
+++ tyrimas/a.py
@@ -4,8 +4,8 @@ import re
 import numpy as np
 import matplotlib.pyplot as plt
 
-GUEST_RESULTS = './guest-results/'
-HOST_RESULTS = './host-results/'
+GUEST_RESULTS = './guest-combined/'
+HOST_RESULTS = './host/'
 NUMBER_FINDER = re.compile('[0-9]+')
 
 
@@ -13,40 +13,40 @@ def load_results(path):
     retval = {}
     currently_loaded = set()
     for fname in os.listdir(path):
-        qbits, iter_id = (int(i) for i in NUMBER_FINDER.findall(fname))
-        assert (qbits, iter_id) not in currently_loaded
-        currently_loaded.add((qbits, iter_id))
+        qbits, = (int(i) for i in NUMBER_FINDER.findall(fname))
+        assert qbits not in currently_loaded
+        currently_loaded.add(qbits)
         fname = os.path.join(path, fname)
         if os.path.isfile(fname):
             with open(fname, 'r') as f:
-                line = [l for l in f.readlines() if 'Time taken' in l]
-                if not line:
+                lines = [l for l in f.readlines() if 'Time taken' in l]
+                if not lines:
                     print(f'W: skipping {fname}')
                     continue
-                assert len(line) == 1
-                time_split = line[0].split(':')
-                assert len(time_split) == 2
-                seconds_taken = int(time_split[1].strip()) / 1e9
-                if qbits not in retval:
-                    retval[qbits] = []
-                retval[qbits] += [seconds_taken]
-    print(retval)
+
+                for line in lines:
+                    time_split = line.split(':')
+                    assert len(time_split) == 2
+                    seconds_taken = int(time_split[1].strip()) / 1e9
+                    if qbits not in retval:
+                        retval[qbits] = []
+                    retval[qbits] += [seconds_taken]
     return retval
 
 
 if __name__ == '__main__':
-    results = load_results('./libquantum-results-better')
-    # guest_results = load_results(GUEST_RESULTS)
-    # host_results = load_results(HOST_RESULTS)
+    guest_results = load_results(GUEST_RESULTS)
+    host_results = load_results(HOST_RESULTS)
     for qbits in range(12, 17):
-        # guest = np.array(guest_results[qbits], dtype=np.float64)
-        # host = np.array(host_results[qbits], dtype=np.float64)
-        # plt.hist(guest)
-        arr = results[qbits]
+        guest = np.array(guest_results[qbits], dtype=np.float64)
+        host = np.array(host_results[qbits], dtype=np.float64)
+        arr = host
         mean = np.mean(arr)
         dev = np.std(arr)
+        print(arr)
         print(mean, dev)
-        plt.hist(results[qbits], bins=32, range=(mean - 2*dev, mean + 2*dev))
+        # plt.hist(arr, bins=32, range=(mean - 2*dev, mean + 2*dev))
+        plt.hist(arr, bins=64)
         plt.show()
         # print(f'{np.std(guest)=}, {np.mean(guest)=}')
         # print(f'{np.std(host)=}, {np.mean(host)=}')
